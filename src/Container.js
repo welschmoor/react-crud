7a23b4889f849a17a384c0cd42403e3d5da73ec3
Johannes Welsch <welschmoor@gmail.com>
2021-10-14 00:20:35 +0200

    added form

--- src/Container.js
+++ src/Container.js
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React, {useState} from 'react'
 import './records.scss'
 
 import Header from "./Header.js"
@@ -7,14 +7,32 @@ import Section from "./Section.js"
 import List from "./List.js"
 import Form from "./Form.js"
 
+const records = [
+    {
+        recordName: "Staying Alive",
+        artistName: "Bee Gees",
+        description: "Listen to this when you are in trenches of WWI"
+    },
+    {
+        recordName: "Staying Dead",
+        artistName: "G-Bees",
+        description: "When you don't want to come back to this world"
+    },
+] 
 
 const Container = () => {
+    const [data, setData] = useState(records)
+
+    const submitHandler = entry => {
+        setData([...data, entry])
+    }
+
     return(
         <div>
             <Header />
             <main>
-                <Section headingText="Add a new one" />
-                <Section headingText="Records" children={<List/>}/>
+                <Section headingText="Add a new one" >  <Form onSubmit={submitHandler} />   </Section>
+                <Section headingText="Records" children={<List children={data}/>}/>
             </main>
             
         </div>
--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,36 @@
 
 
-import React from "react"
+import React, {useState} from "react"
 
+const initalEntryState = {
+    recordName: '',
+    artistName: '',
+    description: '',
+}
 
-const Form = () => {
+const Form = ({onSubmit}) => {
+    const [entry, setEntry] = useState(initalEntryState)
+
+    const onChangeHandler = e => {
+        setEntry({...entry, [e.target.name]: e.target.value})
+    }
+
+    const submitHandler = e => {
+        e.preventDefault()
+        onSubmit({...entry})    // we spread it inside {} simply to copy and make it immutable
+        setEntry(initalEntryState)
+    }
+
     return(
-        
+        <form onSubmit={submitHandler}>
+            <label htmlFor="recordName"></label>
+            <input type="text" id="recordName" name="recordName" required onChange={onChangeHandler} value={entry.recordName} />
+            <label htmlFor="artistName"></label>
+            <input type="text" id="artistName" name="artistName" required onChange={onChangeHandler} value={entry.artistName}/>
+            <label htmlFor="description"></label>
+            <textarea id="description" name="description" required onChange={onChangeHandler} value={entry.description}/>
+            <button type="submit">+Add</button>
+        </form>
     )
 }
 
@@ -18,4 +43,4 @@ const Form = () => {
 
 
 
-export default Form.js
+export default Form
